842b337b0dcf2e97790c9fbbd39d0b83f0bc5692
Steven Martinez <stevenmartinez1494@gmail.com>
2022-04-10 19:19:28 -0700

    clear screen, percentage, plusminus, and add decimal functions added

--- functions.js
+++ functions.js
@@ -15,6 +15,29 @@ function createCalculator() {
     }
 }
 
+/*-----------------------takes two numbers and returns results depending on operand--------------*/
+
+function operate(valueOne, operation, valueTwo) {
+
+    var result = 0; 
+
+    switch (operation) {
+
+        case '+': result = parseInt(valueOne) + parseInt(valueTwo);
+            break;
+        case '-': result = parseInt(valueOne) - parseInt(valueTwo);
+            break;
+        case '*': result = parseInt(valueOne) * parseInt(valueTwo);
+            break;
+        case '/': result =  parseInt(valueOne) / parseInt(valueTwo);
+            break;
+        default: 
+            return;  
+        }
+        return result;
+}
+
+
 /*-----------------------Populate Divs and Buttons--------------*/
 //screen
 const screen = document.querySelector('#div-1');
@@ -24,12 +47,14 @@ screen.id = 'screen';
 const plusMinus = document.querySelector('#div-2');
 plusMinus.id = 'plusMinus';
 plusMinus.textContent = '+ / -';
+plusMinus.addEventListener('click', makeNegativeOrPositive);
 
 //percentage 
 
 const percentage = document.querySelector('#div-3');
 percentage.id = 'percentage';
 percentage.textContent = '%';
+percentage.addEventListener('click', makePercentage);
 
 //division 
 
@@ -48,17 +73,19 @@ multiply.textContent = 'x';
 const seven = document.querySelector('#div-6');
 seven.id = 'seven';
 seven.textContent = '7';
-//seven.addEventListener('click', test, false);
+seven.addEventListener('click', displayOnScreen);
 
 //eight
 const eight = document.querySelector('#div-7');
 eight.id = 'eight';
 eight.textContent = '8';
+eight.addEventListener('click', displayOnScreen);
 
 //nine
 const nine = document.querySelector('#div-8');
 nine.id = 'nine';
 nine.textContent = '9';
+nine.addEventListener('click', displayOnScreen);
 
 //subtract
 const subtract = document.querySelector('#div-9');
@@ -69,16 +96,19 @@ subtract.textContent = '-';
 const six = document.querySelector('#div-10');
 six.id = 'six';
 six.textContent = '6';
+six.addEventListener('click', displayOnScreen);
 
 //five
 const five = document.querySelector('#div-11');
 five.id = 'five';
 five.textContent = '5';
+five.addEventListener('click', displayOnScreen);
 
 //four
 const four = document.querySelector('#div-12');
 four.id = 'four';
 four.textContent = '4';
+four.addEventListener('click', displayOnScreen);
 
 //addition
 
@@ -90,16 +120,19 @@ addition.textContent = '+';
 const three = document.querySelector('#div-14');
 three.id = 'three';
 three.textContent = '3';
+three.addEventListener('click', displayOnScreen);
 
 //two
 const two = document.querySelector('#div-15');
 two.id = 'two';
 two.textContent = '2';
+two.addEventListener('click', displayOnScreen);
 
 //one
 const one = document.querySelector('#div-16');
 one.id = 'one';
 one.textContent = '1';
+one.addEventListener('click', displayOnScreen);
 
 //equals
 const equals = document.querySelector('#div-17');
@@ -110,14 +143,88 @@ equals.textContent = '=';
 const zero = document.querySelector('#div-18');
 zero.id = 'zero';
 zero.textContent = '0';
+zero.addEventListener('click', displayOnScreen);
 
 //decimal
 const decimal = document.querySelector('#div-19');
 decimal.id = 'decimal';
 decimal.textContent = '.';
+decimal.addEventListener('click', addDecimal);
 
 
 //clear 
 const clear = document.querySelector('#div-20');
 clear.id = 'clear';
 clear.textContent = 'C';
+clear.addEventListener('click', clearScreen);
+
+/*------------Displays decimal on screen if none--------------*/
+
+function addDecimal() {
+    let screen = document.querySelector('#screen');
+    if (screen.textContent.includes('.')) {
+        return;
+    } else if (screen.textContent.length < 9) {
+    screen.textContent = screen.textContent.concat(this.textContent);
+    }
+}
+
+/*-----------------------Clears display screen--------------*/
+function clearScreen() {
+    let screen = document.querySelector('#screen');
+    screen.textContent = '';
+}
+
+/*-----------------------Displays digits on screen--------------*/
+function displayOnScreen() {
+    let screen = document.querySelector('#screen');   
+    let screenLength = screen.textContent.length;
+    
+    //if the number is zero on screen it will not display multiple zeros
+    if (screen.textContent == '0') {
+        screen.textContent = '';
+    }
+    //prevents number overflow
+    if (screenLength < 9) {
+        screen.textContent = screen.textContent.concat(this.textContent);
+    }
+}
+
+
+/*--------makes numbers on screen negative or positive--------------*/
+
+function makeNegativeOrPositive() {
+
+    let screen = document.querySelector('#screen'); 
+    let currentNum = screen.textContent;
+    //if screen has empty string or 0 or only decimal e.g. '.' or '0.' with no numbers after return nothing 
+    if (currentNum == '' || currentNum == '0' || currentNum == '.' || currentNum == '0.')  {
+        return;
+    //if starts with . e.g. .5 will print 0.5
+    } else if(currentNum.startsWith('.')) {
+        screen.textContent = ('-' + '0').concat(currentNum);
+    //if already negative will turn results positive 
+    } else if (currentNum.includes('-')) {
+        screen.textContent = screen.textContent.replace('-', ' ').trim();
+    //if less than nine digits and negative 
+    } else if (currentNum.length < 9) {
+        screen.textContent = ('-').concat(currentNum);
+    } else if (currentNum.length >= 9) {
+        currentNum = ('-').concat(currentNum.substring(0,8));
+        screen.textContent = currentNum;
+    }
+}
+
+/*--------converts number on screen to a percentage--------------*/
+
+function makePercentage() {
+    let screen = document.querySelector('#screen'); 
+    let percentage = (parseFloat(screen.textContent) / 100);
+    let numOnScreen = percentage.toString();
+
+    if (numOnScreen.includes('e') || numOnScreen == '0' || numOnScreen.length > 9 || screen.textContent == '') {
+        return;
+    }
+    screen.textContent = numOnScreen;
+}
+
